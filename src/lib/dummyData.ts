05771e5d11f2608dde52b6f559c8053bc32967e7
grkndev <gweepcreative@gmail.com>
2025-08-23 22:40:30 +0300

    data

--- src/lib/dummyData.ts
+++ src/lib/dummyData.ts
@@ -1,26 +1,31 @@
-import { Application, ApplicationStatus } from "@/lib/Application.type";
+import { Application, ApplicationStatus, User } from "@/lib/Application.type";
 
-const dummyData: Application[] = [
- {
+const dummyUser: User = {
     id: "1",
-    name: "Example App",
-    description: "Example App Description",
-    website: "https://example.com",
-    status: ApplicationStatus.ACTIVE,
+    name: "John Doe",
+    email: "john.doe@example.com",
     created_at: "2021-01-01",
     updated_at: "2021-01-01",
-    author: {
+}
+
+
+const dummyData: Application[] = [
+    {
         id: "1",
-        name: "John Doe",
-        email: "john.doe@example.com",
+        name: "Example App",
+        description: "Example App Description",
+        website: "https://example.com",
+        status: ApplicationStatus.ACTIVE,
         created_at: "2021-01-01",
         updated_at: "2021-01-01",
-    },
-    client_id: "1234567890",
-    client_secret: "1234567890",
-    callback_url: "https://example.com/callback",
-    scopes: ["example"],
- }
+        author: dummyUser,
+        client_id: "1234567890",
+        client_secret: "1234567890",
+        callback_url: "https://example.com/callback",
+        scopes: ["example"],
+    }
 ]
+
+
 
 export default dummyData
